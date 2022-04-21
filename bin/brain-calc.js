68ed0b07e78e7cf4745410c5a3dc5a0f5143e2c4
Lerrrr <fmariya27292@gmail.com>
2022-04-21 17:21:35 +0300

    brain-calc fix

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,40 +1,51 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
+import readlineSync from "readline-sync";
 
-const userName = readlineSync.question('May I have your name?');
-console.log('Hello,' + userName + '!');
-console.log('What is the result of the expression?');
+console.log("Welcome to the Brain Games!");
+const userName = readlineSync.question("May I have your name?");
+console.log("Hello," + userName + "!");
+console.log("What is the result of the expression?");
 
-// const userAnswer = readlineSync.question('Your answer: ');
-
-const randomNumber = (min, max) => {
-            return Math.floor(Math.random() * (max - min + 1) + min)
-        };
-
-
-console.log(calculate(rndNum1, rndNum2, '+'));
-
-    export function calculate (num1, num2, operator) {
-        const operators = ['+', '-', '*'];
-        const rndOperator = operators[randomNumber(0, 2)];
-        const rndNum1 = randomNumber(1, 100);
-        const rndNum2 = randomNumber(1, 100);
-
-        let userAnswer;
-    switch (operator){
-        case '+':
-            return num1 + num2;
-            console.log(`${num1} + ${num2}`);
-            break;
-        case '-':
-            return num1 - num2;
-        break;
-        case '*':
-            return num1 * num2;
-        break;
-    }
+function calculate(num1, num2, operator) {
+  switch (operator) {
+    case "+":
+      return num1 + num2;
+    case "-":
+      return num1 - num2;
+    case "*":
+      return num1 * num2;
+  }
+}
+function getIsAnswerCorrect(answer, res) {
+  const correctAnswer = +answer;
+  if (correctAnswer === res) {
+    console.log("Correct!");
+    return true;
+  } else {
+    console.log(`${answer} is wrong answer ;(. Correct answer was ${res}`);
+    return false;
+  }
 }
 
-const result = calculate(rndNum1, rndNum2, rndOperator);
+export function gameCalc() {
+  for (let i = 0; i < 3; i += 1) {
+    const randomNumber = (min, max) => {
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    };
+    const operators = ["+", "-", "*"];
+    const rndOperator = operators[randomNumber(0, 2)];
+    const rndNum1 = randomNumber(1, 10);
+    const rndNum2 = randomNumber(1, 10);
 
+    console.log(`Question: ${rndNum1} ${rndOperator} ${rndNum2}`);
 
+    const result = calculate(rndNum1, rndNum2, rndOperator);
+    const userAnswer = readlineSync.question("Your answer: ");
+    const isAnswerCorrect = getIsAnswerCorrect(userAnswer, result);
+    if (isAnswerCorrect === false) {
+      return;
+    }
+  }
+  console.log("Congratulations," + userName + "!");
+}
+gameCalc();
